f7bacefd7885bf1f11f19d17c627149f79696a6d
Howard P. Logsdon <howard@hplogsdon.com>
2015-03-11 17:02:38 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+module.exports = (function() {
+    var express = require('express'),
+        http = require('http'),
+        path = require('path'),
+        parser = require('body-parser'),
+        app = express();
+
+    app.set('port', (process.env.PORT || 3000));
+    app.use(parser.json());
+    app.use(parser.urlencoded({extended: true}));
+    app.use(express.static(__dirname + '/public'));
+
+    app.post('/', function(req, res) {
+        console.log('REQUEST: ', req.body, "\n");
+        res.json({});
+    });
+
+    app.listen(app.get('port'), function() {
+        console.log("listening on " + app.get('port'));
+    });
+
+}).call(this);
